851586f818c0eea543d8fb482a0636f89080e5ce
wnp <chumchumchira@gmail.com>
2024-11-16 09:46:44 +0500

    removed treesitter after effects mainly

--- lualine.lua
+++ lualine.lua
@@ -1,20 +1,20 @@
 package.path = package.path .. ";/home/saifr/.config/nvim/plugin/hooks/?.lua"
-local hooks = require'hooks'
-local hook_files = require'hook_files'
-local utils = require'utilities'
+local hooks = require("hooks")
+local hook_files = require("hook_files")
+local utils = require("utilities")
 
 local fname = utils.file_content(hooks.path .. "/.hook_files/" .. hook_files.MARKER)
 
 hooks.register_autocommands()
-hooks.signs(0,0)
+hooks.signs(0, 0)
 hooks.kill_flag_set(false)
-require'lualine'.setup {
+require("lualine").setup({
 	options = {
 		icons_enabled = false,
-        theme = 'codedark',
-        --component_separators = { left = '', right = ''},
-		component_separators = { left = '>', right = '<'},
-		section_separators = { left = '<', right = '>'},
+		theme = "codedark",
+		--component_separators = { left = '', right = ''},
+		component_separators = { left = ">", right = "<" },
+		section_separators = { left = "<", right = ">" },
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
@@ -26,26 +26,50 @@ require'lualine'.setup {
 			statusline = 1000,
 			tabline = 1000,
 			winbar = 1000,
-		}
+		},
 	},
 	sections = {
-		lualine_a = {'mode'},
-		lualine_b = {'branch', 'diff', 'diagnostics'},
-		lualine_c = { function () return hooks.fname_cleaned() end },
-        lualine_x = { function() return " " end, 'filetype', function() return fname end },
-		lualine_y = {'progress'},
-		lualine_z = {'location'}
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = {
+			function()
+				return hooks.fname_cleaned()
+			end,
+		},
+		lualine_x = {
+			function()
+				return " "
+			end,
+			"filetype",
+			function()
+				return fname
+			end,
+		},
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
 	},
 	inactive_sections = {
-		lualine_a = {'mode'},
-		lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = { function () return hooks.fname_cleaned() end },
-        lualine_x = { function() return " " end, 'filetype', function() return fname end },
-		lualine_y = {'progress'},
-		lualine_z = {'location'}
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = {
+			function()
+				return hooks.fname_cleaned()
+			end,
+		},
+		lualine_x = {
+			function()
+				return " "
+			end,
+			"filetype",
+			function()
+				return fname
+			end,
+		},
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
 	},
 	tabline = {},
 	winbar = {},
 	inactive_winbar = {},
-	extensions = {}
-}
+	extensions = {},
+})
