ddff68250f020947261ae0aaff24630f638b99ba
wnp <chumchumchira@gmail.com>
2024-11-24 14:02:20 +0500

    lualine default colors reinstated

--- lualine.lua
+++ lualine.lua
@@ -9,67 +9,67 @@ hooks.register_autocommands()
 hooks.signs(0, 0)
 hooks.kill_flag_set(false)
 require("lualine").setup({
-	options = {
-		icons_enabled = false,
-		theme = "codedark",
-		--component_separators = { left = '', right = ''},
-		component_separators = { left = ">", right = "<" },
-		section_separators = { left = "<", right = ">" },
-		disabled_filetypes = {
-			statusline = {},
-			winbar = {},
-		},
-		ignore_focus = {},
-		always_divide_middle = true,
-		globalstatus = false,
-		refresh = {
-			statusline = 1000,
-			tabline = 1000,
-			winbar = 1000,
-		},
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = {
-			function()
-				return hooks.fname_cleaned()
-			end,
-		},
-		lualine_x = {
-			function()
-				return " "
-			end,
-			"filetype",
-			function()
-				return fname
-			end,
-		},
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
-	},
-	inactive_sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = {
-			function()
-				return hooks.fname_cleaned()
-			end,
-		},
-		lualine_x = {
-			function()
-				return " "
-			end,
-			"filetype",
-			function()
-				return fname
-			end,
-		},
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
-	},
-	tabline = {},
-	winbar = {},
-	inactive_winbar = {},
-	extensions = {},
+  options = {
+    -- icons_enabled = false,
+    -- theme = "codedark",
+    -- component_separators = { left = "", right = "" },
+    -- component_separators = { left = ">", right = "<" },
+    -- section_separators = { left = "<", right = ">" },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    },
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = {
+      function()
+        return hooks.fname_cleaned()
+      end,
+    },
+    lualine_x = {
+      function()
+        return " "
+      end,
+      "filetype",
+      function()
+        return fname
+      end,
+    },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+  inactive_sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = {
+      function()
+        return hooks.fname_cleaned()
+      end,
+    },
+    lualine_x = {
+      function()
+        return " "
+      end,
+      "filetype",
+      function()
+        return fname
+      end,
+    },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {},
 })
